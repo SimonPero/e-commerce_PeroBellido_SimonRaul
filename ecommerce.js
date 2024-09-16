6ac8454f2b896ff36a6f5091149456cc25a445a4
Simon Pero <simonraulpero@gmail.com>
2024-09-16 15:58:15 -0300

    dynamic products

--- ecommerce.js
+++ ecommerce.js
@@ -1,21 +1,22 @@
 function changeTheme() {
-  const html = document.documentElement;
-  const themeIcon = document.getElementById('themeIcon');
-
-  if (html.getAttribute('data-bs-theme') === 'light') {
-    html.setAttribute('data-bs-theme', 'dark');
-    themeIcon.classList.replace('bi-moon-fill', 'bi-sun-fill');
-  } else {
-    html.setAttribute('data-bs-theme', 'light');
-    themeIcon.classList.replace('bi-sun-fill', 'bi-moon-fill');
+  const classes = document.getElementById("themeIcon").classList
+  const attributes = document.querySelector("html").attributes
+  if (classes[1] === "bi-moon-fill") {
+    classes.remove("bi-moon-fill")
+    classes.add("bi-sun-fill")
+    attributes[1].value = "dark"
+  } else if (classes[1] === "bi-sun-fill") {
+    classes.remove("bi-sun-fill")
+    classes.add("bi-moon-fill")
+    attributes[1].value = "light"
   }
 }
 
-document.querySelector("#title").innerHTML = "Productos"
+document.querySelector("h1").innerHTML = "Productos"
 
 let arrProds = [];
 
-for (let i = 1; i < 9; i++) {
+for (let i = 1; i < 10; i++) {
   const prod = `
     <div class="card d-flex flex-column align-items-center" style="width: 18rem">
       <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg"
