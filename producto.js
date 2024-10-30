476444a15118d9c7cec7e0ab553d1dbb5f2f844a
Simon Pero <simonraulpero@gmail.com>
2024-10-29 22:50:09 -0300

    minor changes

--- producto.js
+++ producto.js
@@ -7,7 +7,6 @@ class Producto {
       this.imagen = imagen
   }
 }
-
 let prod = new Producto("Mustang", "muy caro", 1, 1000000000, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg")
 
 let data = [
@@ -374,28 +373,28 @@ let data = [
 ]
 
 let id = parseInt(window.location.search.split("=")[1])
-const card = data.find((product) => product.id === id);
+const foundProd = data.find((product) => product.id === id);
 
 let counter = 0
 let cardHtml = `
     <div class="card d-flex flex-column align-items-center" style="width: 18rem">
-      <img src="${card.img}"
+      <img src="${foundProd.img}"
           class="card-img-top"
-          alt="imagen ${card.title}"
+          alt="imagen ${foundProd.title}"
           style="object-fit: cover; width: 100%; height: 150px;"
       />
       <div class="card-body d-flex flex-column">
         <p class="card-text">
-          Titulo:${card.title}
+          Titulo:${foundProd.title}
         </p>
         <p class="card-text">
-          Price:${card.price}
+          Price:${foundProd.price}
         </p>
         <p class="card-text">
-          Stock:${card.stock}
+          Stock:${foundProd.stock}
         </p>
         <p class="card-text">
-          Category:${card.category}
+          Category:${foundProd.category}
         </p>
         ${localStorage.getItem("session")
     ?
@@ -405,7 +404,7 @@ let cardHtml = `
             <span class="form-control text-center shadow-none" id="number-counter" >${counter}</span>
             <button class="btn btn-outline-secondary" type="button" id="button-addon2" onclick="addButton()">+</button>
           </div>
-          <button type="button" class="btn btn-dark">Añadir al carrito</button>
+          <button type="button" class="btn btn-dark" onclick="addButtonToCart()">Añadir al carrito</button>
           `
     :
     `<a class="text-decoration-none text-reset" href="./login.html"><button type="button" class="btn btn-dark">Tienes que inicar sesión para comprar</button></a>`
@@ -425,4 +424,29 @@ function addButton() {
 function restButton() {
   counter -= 1
   numberCounter.innerHTML = counter
+}
+
+let cart = JSON.parse(localStorage.getItem("cart"))
+if (!cart) {
+  cart = []
+}
+
+function addButtonToCart() {
+  if (counter <= 0 || counter > foundProd.stock) return;
+
+  const existingItemIndex = cart.findIndex(item => item.prod.id === foundProd.id);
+  if (existingItemIndex > -1) {
+    const existingItem = cart[existingItemIndex];
+    const newQuantity = existingItem.quantity + counter;
+
+    if (newQuantity <= foundProd.stock) {
+      cart[existingItemIndex].quantity = newQuantity;
+    } else {
+      console.warn("Requested quantity exceeds available stock");
+    }
+  } else {
+    cart.push({ prod: foundProd, quantity: counter });
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
